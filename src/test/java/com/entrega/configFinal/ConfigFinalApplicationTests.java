772b49999d5a1648ca0bc330bfc5b75bafa96150
Elias Gatica <gonzaelias95@gmail.com>
2022-02-22 21:52:08 -0300

    config proyecto final

---
+++ src/test/java/com/entrega/configFinal/ConfigFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrega.configFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
